c033ffca08b779f8b37db78abb6eb60ddba1ba02
Burak Kara <burak.kara@akersolutions.com>
2019-12-20 00:13:49 +0100

    Flurl.url

--- Utilities/BWebServiceUtilities/BWebUtilities.cs
+++ Utilities/BWebServiceUtilities/BWebUtilities.cs
@@ -4,8 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Text;
 using BCommonUtilities;
+using Flurl.Http;
 
 namespace BWebServiceUtilities
 {
@@ -87,27 +87,23 @@ namespace BWebServiceUtilities
         public static bool PerformHTTPRequestSynchronously(
             out int _ResultCode,
             out Tuple<string, string>[] _ResultHeaders,
-            out Tuple<string, string, string>[] _ResultCookies,
-            out HttpWebResponse _ResultResponse,
+            out string _ResultResponse,
             out EBResponseContentType _ResultContentType,
             string _Url,
             string _HttpMethod = "GET",
             Tuple<string, string>[] _Headers = null,
-            Tuple<string, string, string>[] _Cookies = null,
             BStringOrStream _Content = null,
-            long _ContentLength = 0,
-            string _ContentType = null,
             Action<string> _ErrorMessageAction = null)
         {
             _ResultCode = 500;
             _ResultHeaders = null;
-            _ResultCookies = null;
             _ResultResponse = null;
             _ResultContentType = EBResponseContentType.None;
 
-            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(_Url);
-            Request.Method = _HttpMethod;
+            var FlurlRequest = _Url.WithHeader("reserved", "common");
+            System.Net.Http.HttpResponseMessage FlurlResponse = null;
 
+            System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage> ResponseTask = null;
             try
             {
                 if (_Headers != null && _Headers.Length > 0)
@@ -115,127 +111,116 @@ namespace BWebServiceUtilities
                     var Headers = new WebHeaderCollection();
                     foreach (var _Header in _Headers)
                     {
-                        Headers.Add(_Header.Item1, _Header.Item2);
+                        FlurlRequest = FlurlRequest.WithHeader(_Header.Item1, _Header.Item2);
                     }
-                    Request.Headers = Headers;
                 }
 
-                if (_Cookies != null && _Cookies.Length > 0)
+                if (_HttpMethod == "POST" || _HttpMethod == "PUT" || _HttpMethod == "PATCH")
                 {
-                    var Cookies = new CookieContainer();
-                    foreach (var _Cookie in _Cookies)
+                    if (_Content.Type == EBStringOrStreamEnum.String)
                     {
-                        Cookies.Add(new Cookie(_Cookie.Item1, _Cookie.Item2) { Domain = _Cookie.Item3 });
+                        switch (_HttpMethod)
+                        {
+                            case "POST":
+                                ResponseTask = FlurlRequest.PostStringAsync(_Content.String);
+                                break;
+                            case "PUT":
+                                ResponseTask = FlurlRequest.PutStringAsync(_Content.String);
+                                break;
+                            case "PATCH":
+                                ResponseTask = FlurlRequest.PatchStringAsync(_Content.String);
+                                break;
+                        }
                     }
-                    Request.CookieContainer = Cookies;
+                    else
+                    {
+                        using (StreamReader Reader = new StreamReader(_Content.Stream))
+                        {
+                            switch (_HttpMethod)
+                            {
+                                case "POST":
+                                    ResponseTask = FlurlRequest.PostStringAsync(Reader.ReadToEnd());
+                                    break;
+                                case "PUT":
+                                    ResponseTask = FlurlRequest.PutStringAsync(Reader.ReadToEnd());
+                                    break;
+                                case "PATCH":
+                                    ResponseTask = FlurlRequest.PatchStringAsync(Reader.ReadToEnd());
+                                    break;
+                            }
+                        }
+                    }
                 }
+                else if (_HttpMethod == "GET")
+                {
+                    ResponseTask = FlurlRequest.GetAsync();
+                }
+                else if (_HttpMethod == "DELETE")
+                {
+                    ResponseTask = FlurlRequest.DeleteAsync();
+                }
+                else if (_HttpMethod == "HEAD")
+                {
+                    ResponseTask = FlurlRequest.HeadAsync();
+                }
+                else if (_HttpMethod == "OPTION")
+                {
+                    ResponseTask = FlurlRequest.OptionsAsync();
+                }
+                else throw new Exception("Invalid method type " + _HttpMethod);
+
+                ResponseTask.Wait();
+                FlurlResponse = ResponseTask.Result;
             }
             catch (Exception e)
             {
                 _ErrorMessageAction?.Invoke("BWebUtilities->PerformHTTPRequestSynchronously: " + e.Message + ", Trace: " + e.StackTrace);
+                return false;
             }
-
-            if (_Content != null && (_HttpMethod == "POST" || _HttpMethod == "PUT" || _HttpMethod == "DELETE"))
+            finally
             {
-                if (_ContentType != null)
-                {
-                    Request.ContentType = _ContentType;
-                }
-                if (_ContentLength > 0)
-                {
-                    Request.ContentLength = _ContentLength;
-                }
-
-                if (_Content.Type == EBStringOrStreamEnum.String)
-                {
-                    try
-                    {
-                        byte[] ContentAsBytes = Encoding.UTF8.GetBytes(_Content.String);
-
-                        using (Stream RequestBody = Request.GetRequestStream())
-                        {
-                            RequestBody.Write(ContentAsBytes, 0, ContentAsBytes.Length);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        _ErrorMessageAction?.Invoke("BWebUtilities->PerformHTTPRequestSynchronously: Request body write failed: " + e.Message + ", Trace: " + e.StackTrace);
-                        return false;
-                    }
-                }
-                else
-                {
-                    using (Stream RequestBody = Request.GetRequestStream())
-                    {
-                        _Content.Stream.CopyTo(RequestBody);
-                    }
-                }
+                ResponseTask?.Dispose();
             }
 
+            
+            var ResponseHeaders = new List<Tuple<string, string>>();
+            foreach (var Header in FlurlResponse.Headers)
+            {
+                string CombinedValues = "";
+                foreach (var Value in Header.Value)
+                {
+                    CombinedValues += (Value + ",");
+                }
+                CombinedValues = CombinedValues.TrimEnd(',');
+
+                ResponseHeaders.Add(new Tuple<string, string>(Header.Key, CombinedValues));
+            }
+
+            string ContentTypeResult = FlurlResponse.GetHeaderValue("Content-Type");
+
+            _ResultCode = (int)FlurlResponse.StatusCode;
+            _ResultHeaders = ResponseHeaders.ToArray();
+            _ResultContentType = GetEnumFromMimeString(FlurlResponse.Content.Headers.ContentType.ToString());
+
+            System.Threading.Tasks.Task<string> ReadContentTask = null;
             try
             {
-                var Response = (HttpWebResponse)Request.GetResponse();
-
-                Tuple<string, string>[] ResponseHeaders = null;
-                if (Response.Headers != null && Response.Headers.Count > 0)
-                {
-                    ResponseHeaders = new Tuple<string, string>[Response.Headers.Count];
-                    for (int j = 0; j < Response.Headers.Count; j++)
-                    {
-                        string CombinedValues = "";
-                        var Values = Response.Headers.GetValues(Response.Headers.Keys[j]);
-                        if (Values != null && Values.Length > 0)
-                        {
-                            foreach (var Value in Values)
-                            {
-                                CombinedValues += (Value + ",");
-                            }
-                            CombinedValues = CombinedValues.TrimEnd(',');
-                        }
-                        ResponseHeaders[j] = new Tuple<string, string>(Response.Headers.Keys[j], CombinedValues);
-                    }
-                }
-                Tuple<string, string, string>[] ResponseCookies = null;
-                if (Response.Cookies != null && Response.Cookies.Count > 0)
-                {
-                    ResponseCookies = new Tuple<string, string, string>[Response.Cookies.Count];
-                    for (int j = 0; j < Response.Cookies.Count; j++)
-                    {
-                        var CurrentCookie = Response.Cookies[j];
-                        if (CurrentCookie != null)
-                        {
-                            ResponseCookies[j] = new Tuple<string, string, string>(CurrentCookie.Name, CurrentCookie.Value, CurrentCookie.Domain);
-                        }
-                    }
-                }
-
-                _ResultCode = (int)Response.StatusCode;
-                _ResultHeaders = ResponseHeaders;
-                _ResultCookies = ResponseCookies;
-                _ResultContentType = GetEnumFromMimeString(Response.ContentType);
-                _ResultResponse = Response;
-                return true;
-            }
-            catch (WebException e)
-            {
-                var Response = (HttpWebResponse)e.Response;
-                if (Response != null)
-                {
-                    _ResultCode = (int)Response.StatusCode;
-                    _ResultHeaders = null;
-                    _ResultCookies = null;
-                    _ResultContentType = GetEnumFromMimeString(Response.ContentType);
-                    _ResultResponse = Response;
-                    return true;
-                }
-                _ErrorMessageAction?.Invoke("BWebUtilities->PerformHTTPRequestSynchrounously: Response is null after WebException. Message: " + e.Message);
-                return false;
+                ReadContentTask = FlurlResponse.Content.ReadAsStringAsync();
+                ReadContentTask.Wait();
             }
             catch (Exception e)
             {
-                _ErrorMessageAction?.Invoke("BWebUtilities->PerformHTTPRequestSynchronously: Response read failed: " + e.Message + ", Trace: " + e.StackTrace);
+                _ErrorMessageAction?.Invoke("BWebUtilities->PerformHTTPRequestSynchronously: " + e.Message + ", Trace: " + e.StackTrace);
                 return false;
             }
+            finally
+            {
+                _ResultResponse = ReadContentTask?.Result;
+                ReadContentTask?.Dispose();
+                FlurlResponse?.Dispose();
+            }
+            
+            return true;
         }
 
         public static void InjectHeadersFromTupleArraysIntoContext(Tuple<string, string>[] _Headers, HttpListenerContext _Context)
