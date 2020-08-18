919dda809d846153ee979a4a46585e4df668d827
Burak Kara <burak.kara@akersolutions.com>
2020-08-18 19:46:43 +0200

    UTF8 Encoding flag

--- Utilities/BWebServiceUtilities/BWebService.cs
+++ Utilities/BWebServiceUtilities/BWebService.cs
@@ -245,6 +245,7 @@ namespace BWebServiceUtilities
                                 }
                                     
                                 Context.Response.ContentType = Response.ResponseContentType;
+                                Context.Response.ContentEncoding = Encoding.UTF8;
 
                                 if (Response.ResponseContent.Type == EBStringOrStreamEnum.String)
                                 {
