69c56884ac403938097ea6b572c444c643713efe
Berkan Uslu <berkanuslu@gmail.com>
2021-04-13 18:09:09 +0200

    UPDATED MongoDB initialization process with detailed description and having a chance for using other options by passing different environment variables.

--- Services/BServiceUtilities-DatabaseService-MongoDB/BServiceUtilities-DatabaseService-MongoDB.cs
+++ Services/BServiceUtilities-DatabaseService-MongoDB/BServiceUtilities-DatabaseService-MongoDB.cs
@@ -9,7 +9,7 @@ namespace BServiceUtilities
     /// 
     /// <para>Required Environment variables:</para>
     /// 
-    /// <para>MONGO_DB_CONNECTION_STRING, MONGO_DB_DATABASE must be provided and valid.</para>
+    /// <para>((MONGODB_CONNECTION_STRING) or (MONGODB_CLIENT_CONFIG, MONGODB_PASSWORD) or (MONGODB_HOST, MONGODB_PORT)) and MONGODB_DATABASE must be provided and valid.</para>
     /// 
     /// </summary>
     public partial class BServiceInitializer
@@ -24,11 +24,44 @@ namespace BServiceUtilities
             /*
             * File service initialization
             */
-            DatabaseService = new BDatabaseServiceMongoDB(RequiredEnvironmentVariables["MONGODB_CLIENT_CONFIG"], RequiredEnvironmentVariables["MONGODB_PASSWORD"], RequiredEnvironmentVariables["MONGODB_DATABASE"],
+            if (!RequiredEnvironmentVariables.ContainsKey("MONGODB_DATABASE"))
+            {
+                LoggingService.WriteLogs(BLoggingServiceMessageUtility.Single(EBLoggingServiceLogType.Critical, "((MONGODB_CONNECTION_STRING) or (MONGODB_CLIENT_CONFIG, MONGODB_PASSWORD) or (MONGODB_HOST, MONGODB_PORT)) and MONGODB_DATABASE must be provided and valid."), ProgramID, "Initialization");
+                return false;
+            }
+
+            if (RequiredEnvironmentVariables.ContainsKey("MONGODB_CLIENT_CONFIG")
+                && RequiredEnvironmentVariables.ContainsKey("MONGODB_PASSWORD"))
+            {
+                DatabaseService = new BDatabaseServiceMongoDB(RequiredEnvironmentVariables["MONGODB_CLIENT_CONFIG"], RequiredEnvironmentVariables["MONGODB_PASSWORD"], RequiredEnvironmentVariables["MONGODB_DATABASE"],
                 (string Message) =>
                 {
                     LoggingService.WriteLogs(BLoggingServiceMessageUtility.Single(EBLoggingServiceLogType.Critical, Message), ProgramID, "Initialization");
                 });
+            }
+            else if (RequiredEnvironmentVariables.ContainsKey("MONGODB_CONNECTION_STRING"))
+            {
+                DatabaseService = new BDatabaseServiceMongoDB(RequiredEnvironmentVariables["MONGODB_CONNECTION_STRING"], RequiredEnvironmentVariables["MONGODB_DATABASE"],
+                    (string Message) =>
+                    {
+                        LoggingService.WriteLogs(BLoggingServiceMessageUtility.Single(EBLoggingServiceLogType.Critical, Message), ProgramID, "Initialization");
+                    });
+            }
+            else if (RequiredEnvironmentVariables.ContainsKey("MONGODB_HOST")
+                && RequiredEnvironmentVariables.ContainsKey("MONGODB_PORT")
+                && int.TryParse(RequiredEnvironmentVariables["MONGODB_PORT"], out int MongoDbPort))
+            {
+                DatabaseService = new BDatabaseServiceMongoDB(RequiredEnvironmentVariables["MONGODB_HOST"], MongoDbPort, RequiredEnvironmentVariables["MONGODB_DATABASE"],
+                    (string Message) =>
+                    {
+                        LoggingService.WriteLogs(BLoggingServiceMessageUtility.Single(EBLoggingServiceLogType.Critical, Message), ProgramID, "Initialization");
+                    });
+            }
+            else
+            {
+                LoggingService.WriteLogs(BLoggingServiceMessageUtility.Single(EBLoggingServiceLogType.Critical, "((MONGODB_CONNECTION_STRING) or (MONGODB_CLIENT_CONFIG, MONGODB_PASSWORD) or (MONGODB_HOST, MONGODB_PORT)) and MONGODB_DATABASE must be provided and valid."), ProgramID, "Initialization");
+                return false;
+            }
 
             if (DatabaseService == null || !DatabaseService.HasInitializationSucceed())
             {
