fbd9be4128656a5e08ae2a4b2bba1fe39671a102
Berkan Uslu <berkanuslu@gmail.com>
2021-05-06 16:28:34 +0200

    ADDED EVMStatus, EProcessStage, EProcessStatus and EUploadProcessStage enums.

---
+++ Utilities/ServiceUtilities_All/Common/Enums.cs
@@ -0,0 +1,36 @@
+﻿namespace ServiceUtilities_All.Common
+{
+    public enum EVMStatus
+    {
+        Available,
+        Busy,
+        NotResponding
+    }
+
+    public enum EProcessStage
+    {
+        Stage0_FileUploaded,
+        Stage1_PullingData,
+        Stage2_ExtractingGeometryInfo,
+        Stage3_OptimizingModel,
+        Stage4_FilteringModel,
+        Stage5_CustomPlatformConvertion,
+        Stage6_UnrealEngineConvertion,
+        Stage7_Completed
+    }
+    public enum EProcessStatus
+    {
+        Idle,
+        Processing,
+        Canceled,
+        Failed,
+        Completed
+    }
+    public enum EUploadProcessStage : int
+    {
+        NotUploaded = 0,
+        Uploaded_Processing = 1,
+        Uploaded_ProcessFailed = 2,
+        Uploaded_Processed = 3
+    }
+}
