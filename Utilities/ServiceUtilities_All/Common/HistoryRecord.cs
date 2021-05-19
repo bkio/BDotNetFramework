24db628a5893dda69ff2c649df094e61a5e921d2
Berkan Uslu <berkanuslu@gmail.com>
2021-05-19 20:08:31 +0200

    ADDED HistoryRecord to ServiceUtilities.Common.

---
+++ Utilities/ServiceUtilities_All/Common/HistoryRecord.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace ServiceUtilities.Common
+{
+    public class HistoryRecord
+    {
+        public const string RECORD_DATE_PROPERTY = "recordDate";
+        public const string RECORD_PROCESS_STAGE_PROPERTY = "recordProcessStage";
+        public const string RECORD_PROCESS_INFO_PROPERTY = "recordProcessInfo";
+
+        [JsonProperty(RECORD_DATE_PROPERTY)]
+        public string RecordDate = Methods.GetUtcNowShortDateAndLongTimeString();
+
+        [JsonProperty(RECORD_PROCESS_STAGE_PROPERTY)]
+        public int RecordProcessStage = (int)EProcessStage.Stage0_FileUpload;
+
+        [JsonProperty(RECORD_PROCESS_INFO_PROPERTY)]
+        public string ProcessInfo = ""
+    }
+}
