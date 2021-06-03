0fe607a40b3968d80ab94032633361cc36749c15
Berkan Uslu <berkanuslu@gmail.com>
2021-06-03 16:45:43 +0200

    UPDATED LayerFilter and TransformOffset classes.

--- Utilities/ServiceUtilities_All/Common/LayerFilter.cs
+++ Utilities/ServiceUtilities_All/Common/LayerFilter.cs
@@ -7,8 +7,16 @@ namespace ServiceUtilities.Common
     public class LayerFilter
     {
         public const string FILTER_TYPE_PROPERTY = "filterType";
+        public const string FILTER_VALUE_PROPERTY = "filterValue";
+        public const string LAYER_NAME_PROPERTY = "layerName";
 
         [JsonProperty(FILTER_TYPE_PROPERTY)]
         public string FilterType = "";
+
+        [JsonProperty(FILTER_VALUE_PROPERTY)]
+        public string FilterValue = "";
+
+        [JsonProperty(LAYER_NAME_PROPERTY)]
+        public string LayerName = "";
     }
 }
--- Utilities/ServiceUtilities_All/Common/TransformOffset.cs
+++ Utilities/ServiceUtilities_All/Common/TransformOffset.cs
@@ -9,13 +9,10 @@ namespace ServiceUtilities.Common
         public const string LOCATION_OFFSET_X_PROPERTY = "locationOffsetX";
         public const string LOCATION_OFFSET_Y_PROPERTY = "locationOffsetY";
         public const string LOCATION_OFFSET_Z_PROPERTY = "locationOffsetZ";
-        public const string QUATERNION_ROTATION_OFFSET_X_PROPERTY = "quaternionRotationOffsetX";
-        public const string QUATERNION_ROTATION_OFFSET_Y_PROPERTY = "quaternionRotationOffsetY";
-        public const string QUATERNION_ROTATION_OFFSET_Z_PROPERTY = "quaternionRotationOffsetZ";
-        public const string QUATERNION_ROTATION_OFFSET_W_PROPERTY = "quaternionRotationOffsetW";
-        public const string SCALE_X_PROPERTY = "scaleX";
-        public const string SCALE_Y_PROPERTY = "scaleY";
-        public const string SCALE_Z_PROPERTY = "scaleZ";
+        public const string ROTATION_OFFSET_X_PROPERTY = "quaternionRotationOffsetX";
+        public const string ROTATION_OFFSET_Y_PROPERTY = "quaternionRotationOffsetY";
+        public const string ROTATION_OFFSET_Z_PROPERTY = "quaternionRotationOffsetZ";
+        public const string UNIFORM_SCALE_PROPERTY = "uniformScale";
 
         [JsonProperty(LOCATION_OFFSET_X_PROPERTY)]
         public float LocationOffsetX = 0;
@@ -26,25 +23,16 @@ namespace ServiceUtilities.Common
         [JsonProperty(LOCATION_OFFSET_Z_PROPERTY)]
         public float LocationOffsetZ = 0;
 
-        [JsonProperty(QUATERNION_ROTATION_OFFSET_X_PROPERTY)]
-        public float QuaternionRotationOffsetX = 0;
+        [JsonProperty(ROTATION_OFFSET_X_PROPERTY)]
+        public float RotationOffsetX = 0;
 
-        [JsonProperty(QUATERNION_ROTATION_OFFSET_Y_PROPERTY)]
-        public float QuaternionRotationOffsetY = 0;
+        [JsonProperty(ROTATION_OFFSET_Y_PROPERTY)]
+        public float RotationOffsetY = 0;
 
-        [JsonProperty(QUATERNION_ROTATION_OFFSET_Z_PROPERTY)]
-        public float QuaternionRotationOffsetZ = 0;
+        [JsonProperty(ROTATION_OFFSET_Z_PROPERTY)]
+        public float RotationOffsetZ = 0;
 
-        [JsonProperty(QUATERNION_ROTATION_OFFSET_W_PROPERTY)]
-        public float QuaternionRotationOffsetW = 0;
-
-        [JsonProperty(SCALE_X_PROPERTY)]
-        public float ScaleX = 0;
-
-        [JsonProperty(SCALE_Y_PROPERTY)]
-        public float ScaleY = 0;
-
-        [JsonProperty(SCALE_Z_PROPERTY)]
-        public float ScaleZ = 0;
+        [JsonProperty(UNIFORM_SCALE_PROPERTY)]
+        public float UniformScale = 0;
     }
 }
