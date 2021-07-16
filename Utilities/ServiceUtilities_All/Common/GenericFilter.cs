c2629fc60ab2e13063967e85b10c6e6de88d0e0d
Berkan Uslu <berkanuslu@gmail.com>
2021-07-16 14:03:58 +0200

    ADDED GenericFilter class for generic regex filter operations.

---
+++ Utilities/ServiceUtilities_All/Common/GenericFilter.cs
@@ -0,0 +1,18 @@
+﻿/// MIT License, Copyright Burak Kara, burak@burak.io, https://en.wikipedia.org/wiki/MIT_License
+
+using Newtonsoft.Json;
+
+namespace ServiceUtilities.Common
+{
+    public class GenericFilter
+    {
+        public const string FILTER_KEY_PROPERTY = "filterKey";
+        public const string FILTER_VALUE_PROPERTY = "filterValue";
+
+        [JsonProperty(FILTER_KEY_PROPERTY)]
+        public string FilterKey = "";
+
+        [JsonProperty(FILTER_VALUE_PROPERTY)]
+        public string FilterValue = "";
+    }
+}
